f79786246aa5f01718d66e740cb192251712fba0
Mihai Ilincescu <mihai@ilincescu.ro>
2018-10-06 16:43:34 +0300

    Revert "adding azure database"
    This reverts commit fb6645560f0414350d635e311cb4f57bb438d122.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express =  require('express');
 var cors = require('cors');
-var sql = require('mssql');
+
 const app = express();
 app.use(cors());
 
@@ -10,31 +10,13 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 });
 
-const config = {
-  user: 'mishu',
-  password: 'dorina-Ursu0488',
-  server: 'mishuserver.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'js-dev-env',
-
-  options: {
-      encrypt: true // Use this if you're on Windows Azure
-  }
-}
-
-sql.connect(config);
-
 app.get('/users', function(req, res) {
-  const request = new sql.Request();
-  request.query('select * from users')
-    .then(result =>{       
-      res.json(result.recordset);
-    });
   // Hard coding for simplicity. Pretend this hits a real database
-  // res.json([
-  //   {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-  //   {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-  //   {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  // ]);
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
 });
 
 app.listen(app.get('port'), function() {
